38273cd2f6cfde96ee4493d06bd24c7367ee1e5e
COUTINHO ALVES <couto.alves@outlook.com>
2026-02-17 15:09:32 -0300

    Update ex011.py

--- ex001/ex011.py
+++ ex001/ex011.py
@@ -12,3 +12,7 @@ class Gafanhoto:
         return  f'{self.nome} é Gafanhot(a) e tem {self.idade} anos de idade.'
 
 # Declaração de Objetos
+g1 = Gafanhoto()
+g1.nome = 'Maria'
+g1.idade = 20
+print(g1.mensagem())
