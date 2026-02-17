a3b6a8cbc5240dee4a15a8071d4f2b3350417c72
COUTINHO ALVES <couto.alves@outlook.com>
2026-02-17 13:11:47 -0300

    Create ex011.py

---
+++ ex001/ex011.py
@@ -0,0 +1,14 @@
+# Declaração de Class
+class Gafanhoto:
+    def __init__(self):
+        self.nome = ""
+        self.idade = 0
+
+    # Métodos de Instância
+    def aniversario(self):
+        self.idade += 1
+
+    def mensagem(self):
+        return  f'{self.nome} é Gafanhot(a) e tem {self.idade} anos de idade.'
+
+# Declaração de Objetos
